3333df7d539ee5af057f356de3482889e9a7c346
merabza <merabza@gmail.com>
2024-11-16 22:59:26 +0400

    202411162257

--- WebAgent/Program.cs
+++ WebAgent/Program.cs
@@ -11,18 +11,19 @@ using SwaggerTools;
 using SystemToolsShared;
 using WebInstallers;
 
-
 const string appName = "WebAgent";
+const string appKey = "E0FFB24C-7561-4DBA-8E0F-02CA585A3C9C";
 
 //პროგრამის ატრიბუტების დაყენება 
-ProgramAttributes.Instance.SetAttribute("AppName", appName);
+ProgramAttributes.Instance.AppName = appName;
+ProgramAttributes.Instance.AppName = appKey;
 
 try
 {
     var parameters = new Dictionary<string, string>
     {
         //{ SignalRMessagesInstaller.SignalRReCounterKey, string.Empty },//Allow SignalRReCounterKey
-        { ConfigurationEncryptInstaller.AppKeyKey, "E0FFB24C-7561-4DBA-8E0F-02CA585A3C9C" },
+        { ConfigurationEncryptInstaller.AppKeyKey, appKey },
         { SwaggerInstaller.AppNameKey, appName },
         { SwaggerInstaller.VersionCountKey, 1.ToString() }
         //{ SwaggerInstaller.UseSwaggerWithJwtBearerKey, string.Empty },//Allow Swagger
